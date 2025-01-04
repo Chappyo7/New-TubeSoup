f49d646c9ea02f1a8061571e2f871892b3b36bab
Chappyo7 <130234109+Chappyo7@users.noreply.github.com>
2025-01-03 19:44:09 -0500

    Add initial config files

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['i.ytimg.com', 'img.youtube.com'],
+  },
+  experimental: {
+    serverActions: true
+  }
+}
+
+module.exports = nextConfig
